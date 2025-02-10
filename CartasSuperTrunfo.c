47b8d23f3416851faa7821ccd83fbe19be662f07
HenryWillians <henryjunior1999@live.com>
2025-02-10 11:07:39 +0000

    mostrar cartas super trunfo (com bug)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@
 //Teste larissa
 
 int main() {
+    //variáveis
     char pais[20];
     char estado[20];
     char cidade[20];
@@ -15,16 +16,26 @@ int main() {
     int p_turisticos;
 
 
-
+    //cadastro da carta 
     printf("Desafio Super Trunfo:\n");
-
     printf("Cadastre suas cartas: \n");
-
     printf("Vamos começar: \nDigite abaixo o nome do país:");
+    scanf("%s", pais);
+    printf("Digite o estado: \n");
+    scanf("%s", estado);
+    printf("Digite a cidade: \n");
+    scanf("%s", cidade);
+    printf("Insira a área: \n");
+    scanf("%e", area);
+    printf("Insira o PIB:\n");
+    scanf("%f", pib);
+    printf("Quantos pontos turísticos a cidade tem?\n");
+    scanf("%d", p_turisticos);
+
+    // mostrar os dados da carta cadastrada
+    printf("Carta cadastrada com sucesso\n");
+    printf("País: %s \nEstado: %s \nCidade: %s",pais, estado, cidade);
     
-
-
-
 
 
 
