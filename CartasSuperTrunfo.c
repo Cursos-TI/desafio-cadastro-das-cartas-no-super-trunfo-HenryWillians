0f995638dd43eb6ebc0d44282a1afe4f5526e9d5
HenryWillians <henryjunior1999@live.com>
2025-02-13 10:27:00 +0000

    add.calculos módulo intermediário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,32 +12,45 @@ int main() {
     char estado[20];
     char cidade[20];
     float area;
-    double pib;
+    float pib;
     int p_turisticos;
+    int populacao;
+    double renda_per_capta;
+    double densidade_demografica;
 
 
     //cadastro da carta 
-    printf("Desafio Super Trunfo:\n");
+    printf("***Desafio Super Trunfo***\n");
     printf("Cadastre suas cartas: \n");
-    printf("Vamos começar: \nDigite abaixo o nome do país:");
-    scanf("%s", pais);
-    printf("Digite o estado: \n");
-    scanf("%s", estado);
+    printf("Vamos começar: \nDigite abaixo o nome do país:\n");
+    scanf("%s", &pais);
+    printf("Digite o estado (sigla): \n");
+    scanf("%s", &estado);
     printf("Digite a cidade: \n");
-    scanf("%s", cidade);
-    printf("Insira a área: \n");
-    scanf("%e", area);
+    scanf("%s", &cidade);
+    printf("Qual a população de %s-%s?\n", cidade, estado);
+    scanf("%d", &populacao);
+    printf("Insira a área(em km²): \n");
+    scanf("%f", &area);
     printf("Insira o PIB:\n");
-    scanf("%f", pib);
+    scanf("%f", &pib);
     printf("Quantos pontos turísticos a cidade tem?\n");
-    scanf("%d", p_turisticos);
+    scanf("%d", &p_turisticos);
+
+    //cálculo de densidade demográfica e renda-per-capta / ano
+    densidade_demografica = (float)populacao / area;
+    renda_per_capta = pib / (float) populacao;
 
     // mostrar os dados da carta cadastrada
     printf("Carta cadastrada com sucesso\n");
-    printf("País: %s \nEstado: %s \nCidade: %s",pais, estado, cidade);
-    
+    printf("País: %s \nCidade: %s-%s\n",pais, cidade, estado);
+    printf("Os atributos são:\n");
+    printf("- População: %d\n- Área: %.2fkm²\n- PIB: %f\n- Pontos turísticos(Qtd): %d\n- Densidade demográfica: %e/km²\n- Renda-per-capta: %.2f\n", populacao, area, pib, p_turisticos, densidade_demografica, renda_per_capta);
+
+
 
 
 
     return 0;
+    
 }
